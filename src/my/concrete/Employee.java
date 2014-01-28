8f6da3d2cb871ed8df0b0f061a215b3636126801
Justin <Justin@Justin-PC>
2014-01-28 15:31:48 -0600

    first commit

---
+++ src/my/concrete/Employee.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package my.concrete;
+
+/**
+ *
+ * @author Justin
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String dateHired;
+    
+    
+}
